0840826d658f11224baf1b13ed9b3093fb64f243
goto0214 <93575477+goto0214@users.noreply.github.com>
2022-04-13 00:07:20 +0900

    [Update]

--- 3.rb
+++ 3.rb
@@ -1,3 +1,19 @@
 a = gets(chomp: true) + "?"
 #↑文末改行されない
 puts (a + "?")f
+
+
+
+n = gets.to_i
+int = ""
+i = 0
+while i <= 5 do
+    d = n.to_s.reverse
+    n = n + d.to_i
+    int = n.to_s.split('')
+    if int.first == int.last
+        break
+    end
+    i += 1
+end 
+puts n
